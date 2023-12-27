708ad65c66e797bae2a060efd67df9fc5f6f5abc
Carl <youreternity1997@gmail.com>
2023-12-27 14:30:04 +0800

    20231227-Linked List-c++

--- Merge Sort.cpp
+++ Merge Sort.cpp
@@ -8,9 +8,8 @@ void merge(int arr[], int p, int q, int r) {
   // Create L ← A[p..q] and M ← A[q+1..r]
   int n1 = q - p + 1;
   int n2 = r - q;
-
   int L[n1], M[n2];
-
+  
   for (int i = 0; i < n1; i++)
     L[i] = arr[p + i];
   for (int j = 0; j < n2; j++)
