44b4261c0cd020d7955e67405301f35d78d4a6bc
Carl <youreternity1997@gmail.com>
2024-02-03 21:22:16 +0800

    20240203-Stack (Linked List (Class)) (finished)

--- Stack (Linked List (Class)).cpp
+++ Stack (Linked List (Class)).cpp
@@ -1,5 +1,15 @@
 // 將鏈結串列的 head 設為私有變數，以確保外人無法觸及
 // 想要取得堆疊中的資料，唯有透過 getTop()
+#include <iostream>
+using namespace std;
+
+class SLLNode {
+public:
+    int data; // 資料內容
+    SLLNode *next; // 鏈結
+    SLLNode(int _data) : data(_data), next(NULL) {};
+};
+
 class Stack
 {
 private:
@@ -10,8 +20,8 @@ public:
     void push(int); 
     int pop(); 
     int getTop();
+    void printAll();
 };
-
 Stack::Stack() { this -> head = NULL; }
 
 bool Stack::isEmpty() { return this -> head == NULL; }
@@ -57,4 +67,41 @@ int Stack::pop() {
         prev -> next = NULL;
     }
     return toBeReturn;
+}
+
+void Stack::printAll(){
+    SLLNode *cur = head;
+    SLLNode *prev;
+    if (cur==NULL){
+        cout<< "Empty Stack"<< endl;
+        return ;
+    }
+    cout<< "Stack : "; 
+    while (cur) //&& cur -> next != NULL
+    {
+        cout<< cur->data << " -> ";
+        prev = cur;
+        cur = cur -> next;
+    }
+    cout<< "Null "<< endl;
+    return ;
+}
+
+
+int main() {
+    Stack *stack = new Stack;
+    stack -> push(1);
+    stack -> push(2);
+    stack -> push(3);
+    stack -> push(4);
+    stack -> push(5);
+    stack -> printAll();
+
+    stack -> pop();
+    stack -> printAll();
+    
+    stack -> push(6);
+    stack -> printAll();
+
+    return 0;
 }
