7e8b60d8b41c52c2ad592eb407f757b862739fc1
Carl <youreternity1997@gmail.com>
2024-03-29 20:46:23 +0800

    20240329 4. (py/C++)

--- easy/58. Length of Last Word.cpp
+++ easy/58. Length of Last Word.cpp
@@ -1,16 +1,20 @@
+#include <string>
+
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;
-        int j = n - 1;
-        int k = m + n - 1;
-
-        while (j >= 0) {
-            if (i >= 0 && nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
+    int lengthOfLastWord(string s) {
+        int length = 0;
+        int i = s.size() - 1;
+        
+        // Skip trailing spaces
+        while (i >= 0 && s[i] == ' ') {
+            i--;
         }
+        
+        while (i >= 0 && s[i] != ' ') {
+            length++;
+            i--;
+        }
+        return length;
     }
 };
---
+++ hard/4. Median of Two Sorted Arrays (Merge and Sort).py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/median-of-two-sorted-arrays/solutions/4070500/99-journey-from-brute-force-to-most-optimized-three-approaches-easy-to-understand/
+# Brute Force- Merge and Sort
+
+class Solution:
+    def findMedianSortedArrays(self, nums1, nums2):
+        # Merge the arrays into a single sorted array.
+        merged = nums1 + nums2
+
+        # Sort the merged array.
+        merged.sort()
+
+        # Calculate the total number of elements in the merged array.
+        total = len(merged)
+
+        if total % 2 == 1:
+            # If the total number of elements is odd, return the middle element as the median.
+            return float(merged[total // 2])
+        else:
+            # If the total number of elements is even, calculate the average of the two middle elements as the median.
+            middle1 = merged[total // 2 - 1]
+            middle2 = merged[total // 2]
+            return (float(middle1) + float(middle2)) / 2.0
