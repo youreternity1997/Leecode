b4db0dedd51f678c61e1f0e130ca948062979b8a
Carl <youreternity1997@gmail.com>
2024-03-30 20:42:03 +0800

    20240330 206. (py/C++)

--- easy/Linked List/206. Reverse Linked List.cpp
+++ easy/Linked List/206. Reverse Linked List.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
+        ListNode *prev = nullptr;
+        ListNode *curr = head;
         while (curr != nullptr) {
-            ListNode* nextTemp = curr->next;
-            curr->next = prev;
-            prev = curr;
+            ListNode *nextTemp = curr->next;
+            curr->next = prev; // 1 -> None
+            prev = curr; // 1, 2, 3, 4
             curr = nextTemp;
         }
         return prev;
