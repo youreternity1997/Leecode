5ffd307cd0a74781d0607127afce4bb6f4820acc
Carl <youreternity1997@gmail.com>
2024-04-01 23:10:10 +0800

    20240401 217. (Hash Map) (py/C++)

---
+++ easy/Hash Map/217. Contains Duplicate (Hash Map).cpp
@@ -0,0 +1,15 @@
+// time complexity of O(n) 
+// Hash Map
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int, int> seen;
+        for (int num : nums) {
+            if (seen[num] >= 1)
+                return true;
+            seen[num]++;
+        }
+        return false;
+    }
+};
---
+++ easy/Hash Map/217. Contains Duplicate (Hash Map).py
@@ -0,0 +1,11 @@
+# time complexity of O(n) 
+# Hash Map
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen and seen[num] >= 1:
+                return True
+            seen[num] = seen.get(num, 0) + 1
+        return False
