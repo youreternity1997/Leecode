9a3b64c4c79ec399f4f866078715248beb53c4aa
Carl <youreternity1997@gmail.com>
2024-04-06 00:40:32 +0800

    20240406 208. (py/C++)

---
+++ mid/Tree/208. Implement Trie (Prefix Tree).cpp
@@ -0,0 +1,5 @@
+// https://leetcode.com/problems/implement-trie-prefix-tree/solutions/4699425/60-1-approach-1-o-n-python-c-step-by-step-explanation/
+// Time complexity : Insertion: O(m), where m is the length of the wor
+//                   Search: O(m)
+//                   Prefix Search: O(p)
+// Space complexity : O(N * M), where N is the number of words inserted into the Trie, and M is the average length of the words.
