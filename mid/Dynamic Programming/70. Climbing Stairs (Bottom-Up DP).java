46aee3284f4a65f365504a3eb9e8da283a50662c
Carl <youreternity1997@gmail.com>
2024-04-14 23:56:11 +0800

    20240414 70. (Bottom-UP DP) (java)

---
+++ mid/Dynamic Programming/70. Climbing Stairs (Bottom-Up DP).java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/climbing-stairs/solutions/4584184/beats-100-users-c-java-python-javascript-4-approaches-explained/
+// Time complexity : O(n)
+// Space complexity : O(n)
+// Bottom-UP  DP
+
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return solve(0, n, dp);
+    }
+
+    private int solve(int i, int n, int[] dp) {
+        if (i == n) return 1;
+        if (i > n) return 0;
+
+        if (dp[i] != -1) return dp[i];
+
+        dp[i] = solve(i + 1, n, dp) + solve(i + 2, n, dp);
+
+        return dp[i];
+    }
+}
+
