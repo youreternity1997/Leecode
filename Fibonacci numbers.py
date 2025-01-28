103eda91d584e3e62a88e5c9b5f698bb3b34bb54
Carl <youreternity1997@gmail.com>
2025-01-28 10:24:59 +0800

    2025-01-28 10:24:59 edit

--- Fibonacci numbers.py
+++ Fibonacci numbers.py
@@ -2,7 +2,7 @@
 # Edited on 2025-01-26 23:38:30 edit
 # Edited on 2025-01-27 16:11:26 edit
     elif n == 1:
-        return 1;
+# Edited on 2025-01-28 10:24:59 edit
 # Edited on 2025-01-28 10:23:53 edit
     
 print(fibonacci(7))
