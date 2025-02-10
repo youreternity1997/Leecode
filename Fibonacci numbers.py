e81cd58c6793f0bc9107cceebebf744e99051178
Carl <youreternity1997@gmail.com>
2025-02-11 00:44:53 +0800

    2025-02-11 00:44:53 edit

--- Fibonacci numbers.py
+++ Fibonacci numbers.py
@@ -1,7 +1,7 @@
 # Edited on 2025-02-10 22:13:29 edit
 # Edited on 2025-02-10 22:10:46 edit
 # Edited on 2025-01-31 00:02:37 edit
-    elif n == 1:
+# Edited on 2025-02-11 00:44:53 edit
 # Edited on 2025-01-28 10:24:59 edit
 # Edited on 2025-01-28 10:23:53 edit
     
