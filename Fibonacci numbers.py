c918df1c109d71b139417efa35813e3951d1250f
Carl <youreternity1997@gmail.com>
2025-02-11 23:31:48 +0800

    2025-02-11 23:31:45 edit

--- Fibonacci numbers.py
+++ Fibonacci numbers.py
@@ -3,6 +3,6 @@
 # Edited on 2025-01-31 00:02:37 edit
 # Edited on 2025-02-11 00:44:53 edit
 # Edited on 2025-01-28 10:24:59 edit
-# Edited on 2025-01-28 10:23:53 edit
+# Edited on 2025-02-11 23:31:45 edit
     
 print(fibonacci(7))
