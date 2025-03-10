9abf9b0f2ecb796f9979f45324f1040b5f3daff2
Carl <youreternity1997@gmail.com>
2025-03-10 21:10:10 +0800

    133.

--- mid/Graph/133. Clone Graph (DFS+Hash).py
+++ mid/Graph/133. Clone Graph (DFS+Hash).py
@@ -1,19 +1,23 @@
-# https://leetcode.com/problems/clone-graph/solutions/3393172/day-98-dfs-hash-table-easiest-beginner-friendly-sol/
-# Time Complexity : O(N + E), where N is the number of nodes in the graph and E is the number of edges in the graph.
+# https://leetcode.com/problems/clone-graph/solutions/3393494/python-simple-and-clean-beats-94-55/
+# Time Complexity : O(N), where N is the number of nodes in the graph and E is the number of edges in the graph.
 # Space Complexity : O(N), where N is the number of nodes in the graph. 
-# DFS + Hash Table
+# DFS + Stack
+# depth-first search (DFS) algorithm to traverse the graph and create the deep copy.
 
 class Solution:
-    def __init__(self):
-        self.cloningGraph = {}
-        
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
             return None
-        if node in self.cloningGraph:
-            return self.cloningGraph[node]
-        clone = Node(node.val, [])
-        self.cloningGraph[node] = clone
-        for neighbor in node.neighbors:
-            clone.neighbors.append(self.cloneGraph(neighbor))
-        return clone
+        mapping = {}
+        stack = [node]
+        while stack:
+            curr = stack.pop()
+            if curr.val not in mapping:
+                mapping[curr.val] = Node(curr.val)
+            if curr.neighbors != []:
+                for neighbor in curr.neighbors:
+                    if neighbor.val not in mapping:
+                        mapping[neighbor.val] = Node(neighbor.val)
+                        stack.append(neighbor)
+                    mapping[curr.val].neighbors.append(mapping[neighbor.val])
+        return mapping[node.val]
