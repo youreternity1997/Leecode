061efae285d6b6319e7b70be09cd89846b6cde79
Carl <youreternity1997@gmail.com>
2025-04-02 01:16:08 +0800

    20.

--- easy/20. Valid Parentheses/Stack/20_Valid Parentheses.py
+++ easy/20. Valid Parentheses/Stack/20_Valid Parentheses.py
@@ -1,32 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 16 11:33:56 2021
-
-@author: User
-"""
+# https://leetcode.com/problems/valid-parentheses/solutions/6028416/0-ms-runtime-beats-100-user-step-by-steps-solution-easy-to-understand/
+# Time Complexity: (O(n)), where (n) is the length of the string, as each character is pushed and popped from the stack at most once.
+# Space Complexity: (O(n)), for storing unmatched opening brackets in the stack.
+# Beats 100%
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        match = {'{':'}', '(':')', '[':']'}
         stack = []
-    
-        for c in s:
-            if c in match:
-                stack.append(c)
-            else:
-                #print('stack=', stack)
-                #print("stack.pop()=", stack.pop())
-                #print('match[stack.pop()]=', match[stack.pop(-1)])
-                #print('c=', c)
-                if not stack or match[stack.pop()] != c:
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        for char in s:
+            if char in bracket_map:
+                top_element = stack.pop() if stack else '#'
+                if bracket_map[char] != top_element:
                     return False
-
+            else:
+                stack.append(char)
         return not stack
-                
-       
-
-#s = "{()}"
-#s = "{}()[]"
-
-s = "[{()}]"
-print(Solution().isValid(s))
