6c294a301a1dd932b766f747d3ca5b96e1f45ab5
Carl <youreternity1997@gmail.com>
2025-04-04 19:41:02 +0800

    206.

--- easy/206. Reverse Linked List/Linked List/206. Reverse Linked List.py
+++ easy/206. Reverse Linked List/Linked List/206. Reverse Linked List.py
@@ -1,12 +1,22 @@
-class Solution:
-    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev_node = None
-        current_node = head
+# https://leetcode.com/problems/reverse-linked-list/solutions/6550282/0ms-100-step-by-step-visualization-easiest-to-understand-java-c-python/
+# Time complexity: O(n) (linear time).
+# Space complexity:O(1) (constant space).
+# Beats 100%
 
-        while current_node is not None:
-            next_node = current_node.next
-            current_node.next = prev_node
-            prev_node = current_node
-            current_node = next_node
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-        return prev_node
+class Solution(object):
+    def reverseList(self, head):
+        prev = None  # Initialize prev as None
+        curr = head  # Start with curr at the head of the list
+
+        while curr:
+            temp = curr.next  # Store the next node
+            curr.next = prev  # Reverse the pointer
+            prev = curr       # Move prev forward
+            curr = temp      # Move curr forward
+
+        return prev  # prev is the new head of the reversed list
