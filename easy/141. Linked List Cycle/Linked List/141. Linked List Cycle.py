9cfd5eae7d14cf22e5ee11d5978558ff0d1eec9e
Carl <youreternity1997@gmail.com>
2025-04-05 14:19:29 +0800

    141.

--- easy/141. Linked List Cycle/Linked List/141. Linked List Cycle.py
+++ easy/141. Linked List Cycle/Linked List/141. Linked List Cycle.py
@@ -1,20 +1,20 @@
-# https://leetcode.com/problems/linked-list-cycle/
-# Time complexity:0(n)
-# Space complexity:0(1)
+# https://leetcode.com/problems/linked-list-cycle/solutions/6556606/0ms-100-step-by-step-explained-with-visualization-easiest-to-understand-java-c-python/
+# 使用「快慢指針」法（又叫佛洛伊德循環檢測演算法，Floyd’s Cycle-Finding Algorithm）
+# Time complexity:O(n) 最多遍歷整個鏈結串列一次
+# Space complexity: O(1) 只用了兩個指針（無額外空間）
+# Beats 100%
 
-class Solution:
+class Solution(object):
     def hasCycle(self, head):
-        if not head or not head.next: # Empty or One Node
-            return False
-        
-        slow = head
-        fast = head
+        slow = head  # 慢指針
+        fast = head  # 快指針
 
-        while fast.next and fast.next.next:
-            fast = fast.next.next
-            slow = slow.next
+        # 當快指針和其下一個節點不為空時繼續循環
+        while fast is not None and fast.next is not None:
+            slow = slow.next          # 慢指針走一步
+            fast = fast.next.next     # 快指針走兩步
 
-            if slow == fast:
+            if slow == fast:          # 如果兩個指針相遇，代表有環
                 return True
-                
-        return False
+
+        return False  # 若快指針走到底了，代表無環
